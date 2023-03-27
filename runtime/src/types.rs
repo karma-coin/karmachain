2bf4eb41d1a9f40d0f4b76a31b5cb8527bce9a2a
HolyGrease <kireevdanil2000@gmail.com>
2023-03-27 12:56:44 +0300

    fmt

--- runtime/src/types.rs
+++ runtime/src/types.rs
@@ -49,8 +49,10 @@ pub type SignedExtra = (
 /// Unchecked extrinsic type as expected by this runtime.
 pub type UncheckedExtrinsic =
 	generic::UncheckedExtrinsic<Address, RuntimeCall, Signature, SignedExtra>;
+
 /// The payload being signed in transactions.
 pub type SignedPayload = generic::SignedPayload<RuntimeCall, SignedExtra>;
+
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
 	Runtime,
