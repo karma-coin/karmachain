c23a47be08836f4517ace7cd3b4131f01469d7c6
HolyGrease <kireevdanil2000@gmail.com>
2023-03-27 12:04:50 +0300

    remove duplicated staff

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -59,7 +59,7 @@ pub mod types;
 pub mod validators_rewards;
 
 pub use crate::{
-	pallets::{babe::EpochDuration, election_provider_multi_phase::*, identity::*},
+	pallets::{babe::EpochDuration, election_provider_multi_phase::*, identity::*, system::*,},
 	types::*,
 };
 
@@ -148,22 +148,6 @@ pub const KCOINS: Balance = KCENTS * 1_000_000;
 #[cfg(feature = "std")]
 pub fn native_version() -> NativeVersion {
 	NativeVersion { runtime_version: VERSION, can_author_with: Default::default() }
-}
-
-const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
-
-parameter_types! {
-	pub const BlockHashCount: BlockNumber = 2400;
-	pub const Version: RuntimeVersion = VERSION;
-	/// We allow for 2 seconds of compute with a 6 second average block time.
-	pub BlockWeights: frame_system::limits::BlockWeights =
-		frame_system::limits::BlockWeights::with_sensible_defaults(
-			Weight::from_parts(2u64 * WEIGHT_REF_TIME_PER_SECOND, u64::MAX),
-			NORMAL_DISPATCH_RATIO,
-		);
-	pub BlockLength: frame_system::limits::BlockLength = frame_system::limits::BlockLength
-		::max_with_normal_ratio(5 * 1024 * 1024, NORMAL_DISPATCH_RATIO);
-	pub const SS58Prefix: u8 = 42;
 }
 
 impl<C> frame_system::offchain::SendTransactionTypes<C> for Runtime
