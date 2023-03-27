9d64491664d951b46267814188eccfdad0ff3501
HolyGrease <kireevdanil2000@gmail.com>
2023-03-27 14:37:11 +0300

    doc for node crate

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -1,3 +1,9 @@
+//! Responsible for running the blockchain network by coordinating the activities of the network's
+//! participants, validating incoming transactions, and creating new blocks based on Runtime.
+//!
+//! Also provide helpful cli features for running node, syncing node, createing chain specification
+//! file, creating keypairs, etc.
+
 pub mod chain_spec;
 pub mod rpc;
 pub mod service;
