4d0c10f180a3fc2673b94f41a54617430642080b
HolyGrease <kireevdanil2000@gmail.com>
2023-04-17 11:53:03 +0300

    remove redundant staff

--- primitives/common/src/traits.rs
+++ primitives/common/src/traits.rs
@@ -4,12 +4,3 @@ pub trait TransactionIndexer<AccountId> {
 	/// Index currently executing transaction
 	fn index_transaction(account_id: AccountId) -> DispatchResult;
 }
-
-pub trait ExtrinsicData<Signature, AccountId> {
-	///
-	fn signature(self) -> Option<Signature>;
-	///
-	fn signer(self) -> Option<AccountId>;
-	///
-	fn receiver(self) -> Option<AccountId>;
-}
