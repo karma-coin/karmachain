86761352730298a2e92739dd97195dfd2e9859c3
HolyGrease <kireevdanil2000@gmail.com>
2023-05-04 10:45:09 +0300

    signup reward test

--- runtime/tests/common.rs
+++ runtime/tests/common.rs
@@ -6,34 +6,59 @@ mod utils;
 
 /// Contains tests for `EXISTENTIAL_DEPOSIT` (minimum amount of tokens needed for active account).
 mod existential_deposit {
-    use frame_support::assert_err;
-    use sp_core::sr25519;
-    use karmachain_node_runtime::{EXISTENTIAL_DEPOSIT, Runtime, Appreciation, RuntimeOrigin};
-    use crate::utils::*;
-    use sp_common::identity::AccountIdentity;
+	use crate::utils::*;
+	use frame_support::{assert_err, traits::GenesisBuild};
+	use karmachain_node_runtime::{Appreciation, Runtime, RuntimeOrigin, EXISTENTIAL_DEPOSIT};
+	use runtime_api::identity::runtime_decl_for_IdentityApi::IdentityApiV1;
+	use sp_common::identity::AccountIdentity;
+	use sp_core::sr25519;
 
-    /// Any tokens transfer that lead sender's balance falls below `EXISTENTIAL_DEPOSIT`
-    /// should fail.
-    ///
-    /// This test creates account with minimal possible balance and tries to transfer
-    /// `1` token, expecting this lead to `ExistentialDeposit` error.
-    #[test]
-    fn transfer_tokens_below_existential_deposit_should_fail() {
-        new_test_ext()
-            .with_user("Alice", "Bob", "1111")
-            .with_balance("Bob", EXISTENTIAL_DEPOSIT)
-            .execute_with(|| {
-                let who = get_account_id_from_seed::<sr25519::Public>("Bob");
-                let to = get_account_id_from_seed::<sr25519::Public>("Alice");
+	/// Any tokens transfer that lead sender's balance falls below `EXISTENTIAL_DEPOSIT`
+	/// should fail.
+	///
+	/// This test creates account with minimal possible balance and tries to transfer
+	/// `1` token, expecting this lead to `ExistentialDeposit` error.
+	#[test]
+	fn transfer_tokens_below_existential_deposit_should_fail() {
+		new_test_ext()
+			.with_user("Alice", "Bob", "1111")
+			.with_balance("Bob", EXISTENTIAL_DEPOSIT)
+			.execute_with(|| {
+				let who = get_account_id_from_seed::<sr25519::Public>("Bob");
+				let to = get_account_id_from_seed::<sr25519::Public>("Alice");
 
-                // Use appreciation tx to perform transfer
-                assert_err!(Appreciation::appreciation(
-                    RuntimeOrigin::signed(who),
-                    AccountIdentity::AccountId(to),
-                    1, // Transfer 1 Kcent
-                    None,
-                    None
-                ), pallet_balances::Error::<Runtime>::ExistentialDeposit);
-            });
-    }
+				// Use appreciation tx to perform transfer
+				assert_err!(
+					Appreciation::appreciation(
+						RuntimeOrigin::signed(who),
+						AccountIdentity::AccountId(to),
+						1,    // Transfer 1 Kcent
+						None, // No `char_trait`, means appreciation work like transfer tx
+						None
+					),
+					pallet_balances::Error::<Runtime>::ExistentialDeposit
+				);
+			});
+	}
+
+	/// In order to avoid `ExistentialDeposit` error when registering user,
+	/// user receives signup reward. Checks that this reward has been credited.
+	#[test]
+	fn signup_reward_works() {
+		let mut test_executor = new_test_ext();
+
+		test_executor.execute_with(|| {
+			// Rewards values are being configured throw `chainSpec` file
+			// so here we use default values instead of specific `chainSpec` file.
+			pallet_reward::GenesisConfig::<Runtime>::default().build()
+		});
+
+		test_executor.with_user("Alice", "Bob", "1111").execute_with(|| {
+			let account_id = get_account_id_from_seed::<sr25519::Public>("Bob");
+			let info = Runtime::get_user_info_by_account(account_id).expect("Fail to get info");
+
+			// Default signup reward to  10_000_000 KCents
+			assert_eq!(info.balance, 10_000_000)
+		});
+	}
 }
