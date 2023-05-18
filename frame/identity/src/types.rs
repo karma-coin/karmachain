e12d7bc2a7d344438490929da0dbbe9cdb675bce
HolyGrease <kireevdanil2000@gmail.com>
2023-05-18 10:49:50 +0300

    fix after merge

--- frame/identity/src/types.rs
+++ frame/identity/src/types.rs
@@ -3,7 +3,7 @@ use scale_info::TypeInfo;
 
 #[derive(Default, Decode, Encode, MaxEncodedLen, TypeInfo)]
 pub struct UserVerificationData<PublicKey, AccountId, Username, PhoneNumber> {
-	pub verifier_account_id: PublicKey,
+	pub verifier_public_key: PublicKey,
 	pub account_id: AccountId,
 	pub username: Username,
 	pub phone_number: PhoneNumber,
