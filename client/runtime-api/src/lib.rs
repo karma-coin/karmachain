a06c94bce672fc3d801211cc366c7843c6fb907e
HolyGrease <kireevdanil2000@gmail.com>
2023-08-11 10:26:32 +0300

    runtime API interface declaration

--- client/runtime-api/src/lib.rs
+++ client/runtime-api/src/lib.rs
@@ -3,5 +3,6 @@
 pub mod chain;
 pub mod events;
 pub mod identity;
+pub mod nomination_pools;
 pub mod transactions;
 pub mod verifier;
---
+++ client/runtime-api/src/nomination_pools.rs
@@ -0,0 +1,32 @@
+use codec::Codec;
+use pallet_nomination_pools::PoolId;
+use sp_rpc::{BondedPoolInner, NominationPoolsConfiguration};
+use sp_std::vec::Vec;
+
+sp_api::decl_runtime_apis! {
+	/// Runtime api for accessing information about nomination pools.
+	pub trait NominationPoolsApi<AccountId, Balance, BlockNumber>
+		where
+			AccountId: Codec,
+			Balance: Codec,
+			BlockNumber: Codec,
+	{
+		/// Returns the pending rewards for the member that the AccountId was given for.
+		fn pending_rewards(who: AccountId) -> Balance;
+
+		/// Returns the equivalent balance of `points` for a given pool.
+		fn points_to_balance(pool_id: PoolId, points: Balance) -> Balance;
+
+		/// Returns the equivalent points of `new_funds` for a given pool.
+		fn balance_to_points(pool_id: PoolId, new_funds: Balance) -> Balance;
+
+		/// Returns the information about nomination pools
+		fn get_pools(from_index: Option<u32>, limit: Option<u32>) -> Vec<BondedPoolInner<AccountId, Balance, BlockNumber>>;
+
+		/// Return the information about pallet nomination-pools configuration
+		fn get_configuration() -> NominationPoolsConfiguration<Balance>;
+
+		/// Returns the pool id of the pool that the account is a member of
+		fn member_of(account_id: AccountId) -> Option<PoolId>;
+	}
+}
