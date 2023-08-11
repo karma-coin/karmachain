e05b85015cfbfb214994ed1125f44b39a30fe862
HolyGrease <kireevdanil2000@gmail.com>
2023-08-11 09:03:56 +0300

    clean up

--- primitives/rpc/src/lib.rs
+++ primitives/rpc/src/lib.rs
@@ -1,12 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub mod api;
 pub mod chain;
 pub mod identity;
 pub mod transactions;
 pub mod verifier;
 
-pub use api::*;
 pub use chain::*;
 pub use identity::*;
 pub use transactions::*;
