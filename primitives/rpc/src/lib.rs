b21c584b4aa91cdb9fb4f2832f927499a170db45
HolyGrease <kireevdanil2000@gmail.com>
2023-08-11 09:02:36 +0300

    reorganize rpc primitives

--- primitives/rpc/src/lib.rs
+++ primitives/rpc/src/lib.rs
@@ -1,7 +1,13 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod api;
-pub mod types;
+pub mod chain;
+pub mod identity;
+pub mod transactions;
+pub mod verifier;
 
 pub use api::*;
-pub use types::*;
+pub use chain::*;
+pub use identity::*;
+pub use transactions::*;
+pub use verifier::*;
