dcd2278813487a4209d2e90324d862850df34d00
HolyGrease <kireevdanil2000@gmail.com>
2023-09-11 13:08:30 +0300

    fix era issue & rename dev to fast-runtime

--- runtime/src/consts.rs
+++ runtime/src/consts.rs
@@ -11,10 +11,7 @@ pub const MILLISECS_PER_BLOCK: u64 = 12_000;
 // NOTE: Currently it is not possible to change the slot duration after the chain has started.
 //       Attempting to do so will brick block production.
 pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
-#[cfg(not(feature = "dev"))]
-pub const EPOCH_DURATION_IN_SLOTS: BlockNumber = HOURS;
-#[cfg(feature = "dev")]
-pub const EPOCH_DURATION_IN_SLOTS: BlockNumber = MINUTES;
+pub const EPOCH_DURATION_IN_SLOTS: BlockNumber = prod_or_fast!(HOURS, MINUTES);
 pub const ERA_DURATION_IN_EPOCH: u32 = 6;
 
 // Time is measured by number of blocks.
