61b7281d5700a2107e9c824bb36d37e349a9d9fd
HolyGrease <kireevdanil2000@gmail.com>
2023-09-17 20:36:42 +0300

    group dev accounts in backup & update chain specs

--- node/src/chain_spec/backup.rs
+++ node/src/chain_spec/backup.rs
@@ -1,3 +1,4 @@
+use crate::chain_spec::backup::backup_json::User;
 use karmachain_node_runtime::{AccountId, Balance, PhoneNumberHash, Username};
 use pallet_appreciation::CommunityRole;
 use scale_info::prelude::string::String;
@@ -32,12 +33,15 @@ impl BackupGenesisConfig {
 		let treasury = block_producer.balance;
 
 		// Filter users with zero balance and skip validator account
-		let users: Vec<_> = backup
+		let mut users: Vec<_> = backup
 			.users
 			.into_iter()
 			.filter(|info| info.balance > 0)
 			.filter(|info| info.mobile_number.is_some())
 			.collect();
+
+		// Group all developer accounts into one
+		Self::group_dev_accounts(&mut users);
 
 		// Read endowed accounts
 		let endowed_accounts =
@@ -107,6 +111,44 @@ impl BackupGenesisConfig {
 			.collect();
 
 		Ok(Self { endowed_accounts, identities, community_membership, trait_scores, treasury })
+	}
+
+	// Group all developer accounts by adding their balances to main developer account
+	fn group_dev_accounts(users: &mut Vec<User>) {
+		// Main developer account where all funds go
+		const DEV_MAIN_ACCOUNT: &str = "+972549805390";
+		// Dev account from where balance is moved to main developer account
+		const DEV_ACCOUNTS: [&str; 9] = [
+			"+972549805381",
+			"+972549805382",
+			"+972549805383",
+			"+972549805384",
+			"+972549805385",
+			"+972549805386",
+			"+972549805387",
+			"+972549805388",
+			"+972549805389",
+		];
+
+		// Remove all developer accounts and count total balance
+		let total_balance = DEV_ACCOUNTS.into_iter().fold(0, |mut acc, phone_number| {
+			// There are several developer accounts with same phone number
+			while let Some(index) = users
+				.iter()
+				.position(|info| info.mobile_number.as_ref().unwrap().number == phone_number)
+			{
+				let info = users.remove(index);
+				acc += info.balance;
+			}
+			acc
+		});
+
+		let index = users
+			.iter()
+			.position(|info| info.mobile_number.as_ref().unwrap().number == DEV_MAIN_ACCOUNT)
+			.unwrap();
+		let info = users.get_mut(index).unwrap();
+		info.balance += total_balance;
 	}
 }
 
